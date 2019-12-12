eb8595e56175f7a7f234ba65bceb9ec7784318fc
Dachuan Zhao <zhadac@zhadac.home.net>
2019-12-11 22:14:38 -0500

    2nd

--- src/app.py
+++ src/app.py
@@ -9,13 +9,14 @@ def hello():
 @app.route('/upload/', methods=['POST'])
 def upload():
     if request.method == "POST":
-        report_id = request.form.get("report_id")
+        report_id = random.randint(1,100)
         return redirect(url_for('get_report', report_id=report_id))
     return render_template('index.html')
 
 @app.route('/report/<int:report_id>', methods=['POST', 'GET'])
 def get_report(report_id):
-    return render_template('report.html', report_id=report_id)
+    report_id2=  report_id + 10
+    return render_template('report.html', report_id=report_id, report_id2=report_id2)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='5001')
