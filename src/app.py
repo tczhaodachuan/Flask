269608a84c1d793e50a4091dd24d28eb5c5a5a24
Dachuan Zhao <zhadac@zhadac.home.net>
2019-12-17 22:08:41 -0500

    Dynamic images

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for, make_response
 import random
+import os
 app = Flask(__name__, template_folder='views')
 
 @app.route("/")
@@ -18,5 +19,25 @@ def get_report(report_id):
     report_id2=  report_id + 10
     return render_template('report.html', report_id=report_id, report_id2=report_id2)
 
+def generate_image(folder_name):
+    import shutil
+    random_int = int(folder_name)
+    image_name = 'google.png'
+    if random_int % 2 == 0:
+        image_name = 'sample.png'
+    directory = os.path.join('static', str(random_int))
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    shutil.copy(os.path.join('static', image_name), directory)
+    return os.path.join(directory, image_name)
+
+@app.route('/images')
+def get_image():
+    folder_name = request.args.get('filename')
+    full_path = generate_image(folder_name)
+    resp = make_response(open(full_path).read())
+    resp.content_type = "image/jpeg"
+    return resp
+
 if __name__ == '__main__':
-    app.run(debug=True, port='5001')
+    app.run(debug=True)
